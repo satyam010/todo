5c1294b6735c7c4faf7554f7fc1d13a5360271b8
Satyam Prakash <prakashsatyam6@gmail.com>
2020-02-11 19:03:29 +0530

    Updates

---
+++ precache-manifest.3f6068e4c3b49cb2e9840ef54625b456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e8731bb1593db1b8ec2199db73ad9f",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "e20b277ec51c128b8508",
+    "url": "/todo/static/css/main.a6acf22b.chunk.css"
+  },
+  {
+    "revision": "aa4de6e269f327166c94",
+    "url": "/todo/static/js/2.68b2542a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo/static/js/2.68b2542a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20b277ec51c128b8508",
+    "url": "/todo/static/js/main.d70793b7.chunk.js"
+  },
+  {
+    "revision": "dd9adfdb09f83cb1a7aa",
+    "url": "/todo/static/js/runtime-main.f7a45fe9.js"
+  },
+  {
+    "revision": "93326a916609381db6a9b85ae83f3f7f",
+    "url": "/todo/static/media/LCO-logo-white.93326a91.png"
+  }
+]);
